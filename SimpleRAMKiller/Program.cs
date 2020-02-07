5bc8b5f386170ba325d2705a99f184246727f783
septemberEatMe <vitaliiseptember@gmail.com>
2020-02-07 19:50:14 +0200

    ramKiller

---
+++ SimpleRAMKiller/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SimpleRAMKiller
+{
+    class Program
+    {
+        static List<byte[]> buffer = new List<byte[]>();
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                var buf = new byte[1048576];
+                for(int i = 0; i < buf.Length; i++)
+                {
+                    buf[i] = 1;
+                }
+                buffer.Add(buf);
+                Console.WriteLine($"RAM +  {buffer.Count}");
+            }
+        }
+    }
+}
